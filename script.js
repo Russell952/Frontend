4015b6be93e9e258c30d2ed72b56f9d8f3560555
Russell Eshiet <russelleshiet107@gmail.com>
2026-02-26 20:19:40 +0100

    fixed a bug in the hamburger

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.getElementById("hamburger");
+  const navLinks = document.getElementById("nav-links");
+
+  hamburger.addEventListener("click", function () {
+    navLinks.classList.toggle("active");
+  });
+});
+
 // ==========================
 // script.js
 // ==========================
@@ -83,9 +92,3 @@ document.getElementById("contactForm")
     this.reset();
 });
 
-  const hamburger = document.getElementById('hamburger');
-  const navLinks = document.getElementById('nav-links');
-
-    hamburger.addEventListener('click', () => {
-        navLinks.classList.toggle('active'); 
-});
