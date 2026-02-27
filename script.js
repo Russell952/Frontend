af9c0032ac414a1ddb05245245950c960af99c54
Russell Eshiet <russelleshiet107@gmail.com>
2026-02-27 05:10:20 +0100

    updated the UI theme, contact form to support hamburger menu

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const hamburger = document.getElementById("hamburger");
-  const navLinks = document.getElementById("nav-links");
+  const navLinks = document.querySelectorAll(".nav-links");
 
   hamburger.addEventListener("click", function () {
     navLinks.classList.toggle("active");
